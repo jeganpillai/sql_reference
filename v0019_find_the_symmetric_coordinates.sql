418c307267b94db0250e886ee989928f1e9c81e0
jeganpillai <jegan.velappan@gmail.com>
2023-12-27 22:24:33 -0800

    Create v0019_find_the_symmetric_coordinates.sql
    initial version

---
+++ v0019_find_the_symmetric_coordinates.sql
@@ -0,0 +1,37 @@
+-- Question : Find The Symmetric Coordinates
+-- Video: 
+
+-- What is Symmetric Coordinates? 
+-- Two coordindates (X1, Y1) and (X2, Y2) are said to be symmetric coordintes if X1 == Y2 and X2 == Y1.
+-- The solution we are looking at is, among those Symmetric coordinates, get those unique coordinats that satisfy the condition X1 <= Y1.
+
+-- The conditions we have to check: 
+-- 1. X1 == Y2 
+-- 2. X2 == Y1
+-- 3. X1 <= Y1
+
+-- Approach 1: Normal Approach
+SELECT x, y 
+      FROM Coordinates WHERE x = y
+  GROUP BY x
+    HAVING count(x) > 1
+union all 
+SELECT DISTINCT c1.x, c1.y 
+      FROM Coordinates c1 
+INNER JOIN Coordinates c2
+        ON c1.x = c2.y and c1.y = c2.x and c1.x < c1.y
+  ORDER BY 1,2;
+
+-- Approach 2: WINDOWS function 
+with all_data as (
+select x, y
+      ,row_number() over() as rnum 
+      from Coordinates)
+select distinct c1.x, c1.y
+      from all_data c1 
+inner join all_data c2 
+        on c1.x = c2.y 
+       and c2.x = c1.y
+       and c1.x <= c1.y
+       and c1.rnum <> c2.rnum 
+  order by 1,2; 
