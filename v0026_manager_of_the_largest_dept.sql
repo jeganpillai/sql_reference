3def7cf9b91777fb959fb7b24ac3a60ff8c27b8c
jeganpillai <jegan.velappan@gmail.com>
2024-01-11 01:31:39 -0800

    Create v0026_manager_of_the_largest_dept.sql
    initial version

---
+++ v0026_manager_of_the_largest_dept.sql
@@ -0,0 +1,25 @@
+-- Question: Find the Manager Of The Largest Department
+-- Video: https://www.youtube.com/watch?v=TEiPds9514Q
+
+-- Approach 1: General CTE Logic
+with total_emp as (
+select dep_id, count(emp_id) as emp_cnt from Employees group by 1)
+select e.emp_name as manager_name
+      ,e.dep_id 
+      from Employees e 
+inner join total_emp t 
+        on t.dep_id = e.dep_id 
+     where e.position = 'Manager'
+       and t.emp_cnt = (select max(emp_cnt) from total_emp)
+  order by 1;
+
+-- Approach 2: Using WINDOWS Function
+with total_emp as (
+select emp_id, emp_name, dep_id, position, count(*) over(partition by dep_id) as emp_cnt
+from Employees )
+select e.emp_name as manager_name
+      ,e.dep_id 
+      from total_emp e 
+     where e.position = 'Manager'
+       and e.emp_cnt = (select max(emp_cnt) from total_emp)
+  order by 1;
