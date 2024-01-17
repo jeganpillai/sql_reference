7fc3c5c13de968be03c733c4ca296b2c74b0388a
jeganpillai <jegan.velappan@gmail.com>
2024-01-17 02:21:28 -0800

    Create v0029_find_employees_not_eligible_for_salary.sql
    initial version

---
+++ v0029_find_employees_not_eligible_for_salary.sql
@@ -0,0 +1,21 @@
+-- Question: Find employees who are not eligible to get their salary
+-- Video: https://www.youtube.com/watch?v=qvpX0Ch2ck8
+
+-- Approach 1: CTE Table Logic
+select employee_id 
+      ,sum(time_to_sec(timediff(out_time, in_time))/3600) as worked_hours 
+      from Logs 
+  group by 1)
+select emp.employee_id
+       from Employees emp
+  left join employee_working_hr wrk
+         on emp.employee_id = wrk.employee_id
+      where ( emp.needed_hours > wrk.worked_hours or wrk.employee_id is null);
+
+-- Approach 2: Direct Left Join
+select emp.employee_id
+      from Employees emp 
+ left join Logs l 
+        on l.employee_id = emp.employee_id
+  group by 1 
+    having max(emp.needed_hours) > coalesce(sum(time_to_sec(timediff(out_time, in_time))/3600),0);
